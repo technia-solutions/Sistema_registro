ca9731605d2cf8bb0f88413b005075f18ed6b876
Carlos <cmchamorro99@hotmail.com>
2020-06-30 14:54:31 -0600

    Agregando conector de la base de datos

---
+++ Sistema_registro/src/sistema_registro/SQL/ConectorSQL.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_registro.SQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Carlos
+ */
+public class ConectorSQL {
+    
+    private static Connection con = null;
+    private String driver;
+    private String url;
+    private String user;
+    private String pass;
+   
+    
+    private ConectorSQL() throws SQLException{
+        
+       String url = "jdbc:sqlserver://CARLOS:1433;"
+               + "databaseName=TechD;user=admin;"
+               + "password=admin";
+       String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+      
+       try{
+       Class.forName(driver);
+       con = DriverManager.getConnection(url);
+       } catch(ClassNotFoundException | SQLException e){
+                e.printStackTrace();
+        }
+    }
+    
+    public static Connection obtenerConexion() throws SQLException{
+        if (con == null){
+            ConectorSQL conectorSQL = new ConectorSQL(); 
+        }
+        return con;
+    }  
+}
