d267fe83514438e1ee30b63944f969fa4affdbec
Carlos <cmchamorro99@hotmail.com>
2020-06-29 20:49:27 -0600

    Prueba 4

--- Sistema_registro/src/sistema_registro/Sistema_registro.java
+++ Sistema_registro/src/sistema_registro/Sistema_registro.java
@@ -16,10 +16,6 @@ public class Sistema_registro {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo 2");
-        System.out.println("Hola Mundo 3");
-        System.out.println("Hola Mundo 4");
-        System.out.println("prueba");
     }
     
 }
