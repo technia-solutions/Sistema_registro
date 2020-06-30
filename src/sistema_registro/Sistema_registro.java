d92fd4ccdafb0c6716fea1566475d7d08fd32df1
asus <gnssmndoza09@gmail.com>
2020-06-29 20:01:31 -0600

    Commit inicial

---
+++ src/sistema_registro/Sistema_registro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_registro;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Sistema_registro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Prueba 1111");
+    }
+    
+}
