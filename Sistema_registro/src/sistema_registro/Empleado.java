32b49a0385cfd55c2d98b344db0686c693fc31aa
Carlos <cmchamorro99@hotmail.com>
2020-07-10 00:43:42 -0600

    Menor - Estetica en pantalla empleado v3

--- Sistema_registro/src/sistema_registro/Empleado.java
+++ Sistema_registro/src/sistema_registro/Empleado.java
@@ -421,13 +421,13 @@ this.cbo_tipoUsuario.setSelectedItem("");
         Tabla_Empleados.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         Tabla_Empleados.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Id Empleado", "Nombres", "Apellidos", "Salario", "Telefono", "Numero identidad", "idCampus", "NombreUsuario", "tipoUsuario"
             }
         ));
         Tabla_Empleados.setToolTipText("Presiona consulta para ver todos los empleados");
