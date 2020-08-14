27116c8354563a560d0a2b193b2c11e3dcba4bd8
technia-solutions <67610332+technia-solutions@users.noreply.github.com>
2020-08-14 11:42:57 -0600

    Correcciones Pantalla Matricula

--- Sistema_registro/src/sistema_registro/Matricula.java
+++ Sistema_registro/src/sistema_registro/Matricula.java
@@ -79,8 +79,8 @@ public class Matricula extends javax.swing.JFrame {
         lbl_cancelarAsignatura = new javax.swing.JButton();
         lbl_periodo = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu = new javax.swing.JMenu();
+        jMenuPrincipal = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -180,12 +180,17 @@ public class Matricula extends javax.swing.JFrame {
             }
         });
 
-        jMenu1.setText("File");
+        lbl_periodo.setText("jLabel1");
 
-        jMenuItem1.setText("jMenuItem1");
-        jMenu1.add(jMenuItem1);
+        jMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/menu.png"))); // NOI18N
+        jMenu.setText("Menu");
 
-        jMenuBar1.add(jMenu1);
+        jMenuPrincipal.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_P, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuPrincipal.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/inicio.png"))); // NOI18N
+        jMenuPrincipal.setText("Menu Principal");
+        jMenu.add(jMenuPrincipal);
+
+        jMenuBar1.add(jMenu);
 
         setJMenuBar(jMenuBar1);
 
@@ -232,7 +237,7 @@ public class Matricula extends javax.swing.JFrame {
                 .addComponent(lbl_periodo)
                 .addGap(43, 43, 43)
                 .addComponent(lbl_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbl_numeroCuenta)
                     .addComponent(txt_numeroCuenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -780,9 +785,9 @@ public class Matricula extends javax.swing.JFrame {
     private javax.swing.JButton btn_cancelarMatricula;
     private javax.swing.JButton btn_generarReporte;
     private javax.swing.JButton btn_matricularAsignatura;
-    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuPrincipal;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton lbl_cancelarAsignatura;
     private javax.swing.JLabel lbl_numeroCuenta;
