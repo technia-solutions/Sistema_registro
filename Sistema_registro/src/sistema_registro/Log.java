e2f7c4ee0cfde99e28261d3b4223b4e2ff0e7102
Carlos <cmchamorro99@hotmail.com>
2020-10-24 18:17:57 -0600

    Minor - Generando clase de logs y agregando logs en Login

---
+++ Sistema_registro/src/sistema_registro/Log.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_registro;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Log {
+    public Logger logger;
+    FileHandler fh;
+    
+    public Log(String fileName) throws SecurityException, IOException{
+        File f = new File(fileName);
+        if(!f.exists()){
+            f.createNewFile();
+        }
+        fh = new FileHandler(fileName,true);
+        logger = Logger.getLogger("test");
+        logger.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fh.setFormatter(formatter);
+    }
+}
--- Sistema_registro/src/sistema_registro/Login.java
+++ Sistema_registro/src/sistema_registro/Login.java
@@ -6,6 +6,7 @@
 package sistema_registro;
 
 import java.awt.Toolkit;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.*;
@@ -284,7 +285,16 @@ public class Login extends javax.swing.JFrame {
             } */
 
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,e.getMessage());
+              
+            try {
+                Log myLog; 
+                myLog = new Log("src\\Logs\\Login.txt");
+                myLog.logger.setLevel(Level.SEVERE);
+                myLog.logger.severe(e.getMessage() + " La causa fue: " + e.getCause());
+            } catch (IOException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                 
         }
 
     }//GEN-LAST:event_btn_ingresarActionPerfomed
